8877d0105baf3cf71b14270116b74a5962c51149
SurenderLohia <surender.lohia.e@gmail.com>
2022-08-19 10:36:16 +0530

    Update

--- src/data.js
+++ src/data.js
@@ -1,295 +1,334 @@
 module.exports = {
-    profile: {
-        name: 'Surender Lohia',
-        title: 'Senior Frontend Developer at Tenantcube',
-        photo: 'https://avatars1.githubusercontent.com/u/6301437?s=460&v=4', // 300x300px
-        city: 'Bangalore, India',
-        website: 'https://www.surender.net/',
-        phone: '9551800431',
-        mail: 'surender.lohia.e@gmail.com'
+  profile: {
+    name: 'Surender Lohia',
+    title: 'Senior Frontend Developer at Assembly',
+    photo: 'https://avatars1.githubusercontent.com/u/6301437?s=460&v=4', // 300x300px
+    city: 'Bangalore, India',
+    website: 'https://www.surender.net/',
+    phone: '9551800431',
+    mail: 'surender.lohia.e@gmail.com',
+  },
+  social: {
+    twitter: {
+      name: '@surender_lohia',
+      link: 'https://twitter.com/surender_lohia',
     },
-    social: {
-        twitter: {
-            name: '@surender_lohia',
-            link: 'https://twitter.com/surender_lohia'
-        },
-        linkedin: {
-            name: 'in/surenderlohia',
-            link: 'https://www.linkedin.com/in/surenderlohia'
-        },
-        github: {
-            name: 'surenderlohia',
-            link: 'https://github.com/surenderlohia'
-        }
+    linkedin: {
+      name: 'in/surenderlohia',
+      link: 'https://www.linkedin.com/in/surenderlohia',
     },
-    summary: `Producing quality apps, focusing on writing eloquent and maintainable code. Working on Front-end, JavaScript, and Mobile App for large Scale applications.`,
-    experiences: [
-        {
-            company: 'Tenantcube',
-            location: 'Bangalore',
-            title: 'Senior Frontend Developer',
-            period: 'MAY 2020 - PRESENT',
-            jobdesc: [
-                'Responsibility to develop large scale application. Coordinate with designer, back-end devs and BA. Front-end tech stack HTML5, CSS3, JavaScript, Sass, React, Redux, Webpack, Npm, Jest.'
-            ],
-        },
-        {
-            company: 'ThoughtWorks',
-            location: 'Bangalore',
-            title: 'Frontend Developer',
-            period: 'MAY 2018 - MAR 2020',
-            jobdesc: [
-                'Responsibility to develop large scale application. Coordinate with designer, back-end devs and BA. Front-end tech stack HTML5, CSS3, JavaScript, Sass, React, Redux, Webpack, Npm, Jest.'
-            ],
-        },
-        {
-            company: 'Tenmiles',
-            location: 'Chennai',
-            title: 'Frontend Developer',
-            period: 'JUN 2015 – MAY 2018',
-            jobdesc: [
-                'Front End and JavaScript development for large scale chat application. Front-end tech stack HTML5, CSS3, JavaScript, Sass, BackboneJS, jQuery, React'
-            ],
-        },
-        {
-            company: 'Fantain Sports',
-            location: 'Chennai',
-            title: 'Frontend Developer',
-            period: 'JAN 2014 – MAY 2015',
-            jobdesc: [
-                'Mobile Application Front-end and JavaScript Development for large scale application. Front-end tech stack HTML5, CSS3, JavaScript, Knockout JS, Require JS, Sass, and Cordova.',
-            ],
-        },
-        {
-            company: 'Rage Communications',
-            location: 'Chennai',
-            title: 'Frontend Developer',
-            period: 'OCT 2012 – DEC 2013',
-            jobdesc: [
-                'Front End Web development with Web Standards and Cross browser compatibility. Coordinate with designer and Back-End team. Front-end tech stack HTML, CSS, JavaScript, jQuery.',
-            ],
-        },
-        {
-            company: 'Sam-Sys',
-            location: 'Chennai',
-            title: 'Frontend Developer',
-            period: 'AUG 2011 – SEP 2012',
-            jobdesc: [
-                'Front End Web development with Web Standards and Cross browser compatibility. Coordinate with designer and Back-End team. Front-end tech stack HTML, CSS, JavaScript, jQuery, Sass.'
-            ]
-        }
-    ],
-    educations: [{
-        name: 'C.Kandhaswami Naidu College for Men',
-        city: 'Chennai',
-        degree: 'Bachelor Degree',
-        faculty: 'Bachelor of Computer Application'
-    }],
-    projects: [
-        {
-            title: 'Fedi-Todo',
-            company: 'Open Source Project',
-            link: 'https://fedi-todo.surge.sh',
-            description: 'Todo List Progressive Web App for Front-end Developer interview preparation.',
-            techStack: 'HTML5, CSS3, JavaScript and Svelte.'
-        },
-        {
-            title: 'HappyFox Chat',
-            company: 'HappyFox',
-            link: 'https://happyfoxchat.com',
-            description: 'HappyFox Chat is a live chat application. My responsibility was Front-end development. HappyFox Chat widget can be installable on client\'s websites after that they can able to chat with there end users. Here I was worked on both Admin module as well as the Chat widget.',
-            techStack: 'HTML5, CSS3, JavaScript, React, Backbone.js, Bootstrap, Node.js, Express, and MongoDB.'
-        },
-        {
-            title: 'World Movies',
-            company: 'Open Source Project',
-            link: 'https://world-movies.surge.sh',
-            description: 'Movies listing app with search and filter functionality. Listed movies are predefined movie collection from a book.',
-            techStack: 'HTML5, CSS3, JavaScript, Vue.js, Bulma, Basscss, and Cypress.'
-        },
-        {
-            title: 'Dude Quotes',
-            company: 'Open Source Project',
-            link: 'https://dude-quotes.surge.sh',
-            description: 'Quotes listing app with the ability to add new quotes by the user.',
-            techStack: 'HTML5, CSS3, JavaScript, Angular, Node, Express and Mongodb.'
-        },
-        {
-            title: 'Fantain',
-            company: 'Fantain',
-            link: 'https://play.google.com/store/apps/details?id=com.fantain.comainapp',
-            description: 'Fantain is the cool new hangout for the sports fan! Sports fans want a lot from the the sports they follow. My responsibility was Front-end development.',
-            techStack: 'HTML5, CSS3, JavaScript, Sass, Knockout.js, Apache Cordova.'
-        },
-        {
-            title: 'Html Boilerplate',
-            company: 'Open Source Project',
-            link: 'https://www.surender.net/html-boilerplate/',
-            description: 'A simple boilerplate code for html. Built with HTML, CSS, and JavaScript.',
-            techStack: 'HTML5, CSS3, JavaScript'
-        }
-    ],
-    skills: [{
-            name: 'JavaScript',
-            category: 'js'
-        },
-        {
-            name: 'CSS',
-            category: 'css'
-        },
-        {
-            name: 'HTML',
-            category: 'html'
-        },
-        {
-            name: 'Sass',
-            category: 'css'
-        },
-        {
-            name: 'JQuery',
-            category: 'js'
-        },
-        {
-            name: 'React',
-            category: 'js'
-        },
-        {
-            name: 'Vue.js',
-            category: 'js'
-        },
-        {
-            name: 'Angular',
-            category: 'js'
-        },
-        {
-            name: 'Redux',
-            category: 'js'
-        },
-        {
-            name: 'Vuex',
-            category: 'js'
-        },
-        {
-            name: 'Svelte',
-            category: 'js'
-        },
-        {
-            name: 'Knockout.js',
-            category: 'js'
-        },
-        {
-            name: 'Backbone.js',
-            category: 'js'
-        },
-        {
-            name: 'TypeScript',
-            category: 'js'
-        },
-        {
-            name: 'D3',
-            category: 'js'
-        },
-        {
-            name: 'Gulp',
-            category: 'tool'
-        },
-        {
-            name: 'Npm',
-            category: 'tool'
-        },
-        {
-            name: 'Webpack',
-            category: 'tool'
-        },
-        {
-            name: 'Mocha',
-            category: 'tool'
-        },
-        {
-            name: 'Jest',
-            category: 'tool'
-        },
-        {
-            name: 'Cypress',
-            category: 'tool'
-        },
-        {
-            name: 'Git',
-            category: 'tool'
-        },
-        {
-            name: 'Unix',
-            category: 'tool'
-        },
-        {
-            name: 'Jekyll',
-            category: 'tool'
-        },
-        {
-            name: 'Bulma',
-            category: 'css'
-        },
-        {
-            name: 'Semantic UI',
-            category: 'css'
-        },
-        {
-            name: 'Material UI',
-            category: 'css'
-        },
-        {
-            name: 'Twitter Bootstrap',
-            category: 'css'
-        },
-        {
-            name: 'Responsive',
-            category: 'tech'
-        },
-        {
-            name: 'Mobile Web App',
-            category: 'tech'
-        },
-        {
-            name: 'PWA',
-            category: 'tech'
-        },
-        {
-            name: 'Node.js',
-            category: 'language'
-        },
-        {
-            name: 'Python',
-            category: 'language'
-        },
-        {
-            name: 'Php',
-            category: 'language'
-        },
-        {
-            name: 'Express.js',
-            category: 'backend-framework'
-        },
-        {
-            name: 'Django',
-            category: 'backend-framework'
-        },
-        {
-            name: 'Wordpress',
-            category: 'backend-framework'
-        },
-        {
-            name: 'SQLite',
-            category: 'database'
-        },
-        {
-            name: 'MongoDB',
-            category: 'database'
-        },
-    ],
-    hobbies: [
-        'Reading',
-        'Coding',
-        'Cycling',
-        'Travelling'
-    ]
-}
+    github: {
+      name: 'surenderlohia',
+      link: 'https://github.com/surenderlohia',
+    },
+  },
+  summary: `Producing quality apps, focusing on writing eloquent and maintainable code. Working on Front-end, JavaScript, and Mobile App for large Scale applications.`,
+  experiences: [
+    {
+      company: 'Assembly',
+      location: 'Bangalore',
+      title: 'Senior Frontend Developer',
+      period: 'OCT 2020 - PRESENT',
+      jobDesc: [
+        'Responsibility to develop large scale application. Coordinate with designer, back-end team and BA. Front-end tech stack HTML5, CSS3, JavaScript, Sass, React, Redux, StyledComponents, ReactQuery, Zustand, PlayWright, Atomic Design, Webpack, Npm, Jest, StoryBook, Figma',
+      ],
+    },
+    {
+      company: 'Tenantcube',
+      location: 'Bangalore',
+      title: 'Senior Frontend Developer',
+      period: 'MAY 2020 - PRESENT',
+      jobDesc: [
+        'Responsibility to develop large scale application. Coordinate with designer, back-end team and BA. Front-end tech stack HTML5, CSS3, JavaScript, Sass, React, Redux, Webpack, Npm, Jest.',
+      ],
+    },
+    {
+      company: 'ThoughtWorks',
+      location: 'Bangalore',
+      title: 'Frontend Developer',
+      period: 'MAY 2018 - MAR 2020',
+      jobDesc: [
+        'Responsibility to develop large scale application. Coordinate with designer, back-end team and BA. Front-end tech stack HTML5, CSS3, JavaScript, Sass, React, Redux, Webpack, Npm, Jest.',
+      ],
+    },
+    {
+      company: 'Tenmiles',
+      location: 'Chennai',
+      title: 'Frontend Developer',
+      period: 'JUN 2015 – MAY 2018',
+      jobDesc: [
+        'Front End and JavaScript development for large scale chat application. Front-end tech stack HTML5, CSS3, JavaScript, Sass, BackboneJS, jQuery, React',
+      ],
+    },
+    {
+      company: 'Fantain Sports',
+      location: 'Chennai',
+      title: 'Frontend Developer',
+      period: 'JAN 2014 – MAY 2015',
+      jobDesc: [
+        'Mobile Application Front-end and JavaScript Development for large scale application. Front-end tech stack HTML5, CSS3, JavaScript, Knockout JS, Require JS, Sass, and Cordova.',
+      ],
+    },
+    {
+      company: 'Rage Communications',
+      location: 'Chennai',
+      title: 'Frontend Developer',
+      period: 'OCT 2012 – DEC 2013',
+      jobDesc: [
+        'Front End Web development with Web Standards and Cross browser compatibility. Coordinate with designer and Back-End team. Front-end tech stack HTML, CSS, JavaScript, jQuery.',
+      ],
+    },
+    {
+      company: 'Sam-Sys',
+      location: 'Chennai',
+      title: 'Frontend Developer',
+      period: 'AUG 2011 – SEP 2012',
+      jobDesc: [
+        'Front End Web development with Web Standards and Cross browser compatibility. Coordinate with designer and Back-End team. Front-end tech stack HTML, CSS, JavaScript, jQuery, Sass.',
+      ],
+    },
+  ],
+  educations: [
+    {
+      name: 'C.Kandhaswami Naidu College for Men',
+      city: 'Chennai',
+      degree: 'Bachelor Degree',
+      faculty: 'Bachelor of Computer Application',
+    },
+  ],
+  projects: [
+    {
+      title: 'Fedi-Todo',
+      company: 'Open Source Project',
+      link: 'https://fedi-todo.surge.sh',
+      description:
+        'Todo List Progressive Web App for Front-end Developer interview preparation.',
+      techStack: 'HTML5, CSS3, JavaScript and Svelte.',
+    },
+    {
+      title: 'HappyFox Chat',
+      company: 'HappyFox',
+      link: 'https://happyfoxchat.com',
+      description:
+        "HappyFox Chat is a live chat application. My responsibility was Front-end development. HappyFox Chat widget can be installable on client's websites after that they can able to chat with there end users. Here I was worked on both Admin module as well as the Chat widget.",
+      techStack:
+        'HTML5, CSS3, JavaScript, React, Backbone.js, Bootstrap, Node.js, Express, and MongoDB.',
+    },
+    {
+      title: 'World Movies',
+      company: 'Open Source Project',
+      link: 'https://world-movies.surge.sh',
+      description:
+        'Movies listing app with search and filter functionality. Listed movies are predefined movie collection from a book.',
+      techStack:
+        'HTML5, CSS3, JavaScript, Vue.js, Bulma, Basscss, and Cypress.',
+    },
+    {
+      title: 'Dude Quotes',
+      company: 'Open Source Project',
+      link: 'https://dude-quotes.surge.sh',
+      description:
+        'Quotes listing app with the ability to add new quotes by the user.',
+      techStack: 'HTML5, CSS3, JavaScript, Angular, Node, Express and Mongodb.',
+    },
+    {
+      title: 'Fantain',
+      company: 'Fantain',
+      link: 'https://play.google.com/store/apps/details?id=com.fantain.comainapp',
+      description:
+        'Fantain is the cool new hangout for the sports fan! Sports fans want a lot from the the sports they follow. My responsibility was Front-end development.',
+      techStack: 'HTML5, CSS3, JavaScript, Sass, Knockout.js, Apache Cordova.',
+    },
+    {
+      title: 'Html Boilerplate',
+      company: 'Open Source Project',
+      link: 'https://www.surender.net/html-boilerplate/',
+      description:
+        'A simple boilerplate code for html. Built with HTML, CSS, and JavaScript.',
+      techStack: 'HTML5, CSS3, JavaScript',
+    },
+  ],
+  skills: [
+    {
+      name: 'JavaScript',
+      category: 'js',
+    },
+    {
+      name: 'CSS',
+      category: 'css',
+    },
+    {
+      name: 'HTML',
+      category: 'html',
+    },
+    {
+      name: 'Sass',
+      category: 'css',
+    },
+    {
+      name: 'JQuery',
+      category: 'js',
+    },
+    {
+      name: 'React',
+      category: 'js',
+    },
+    {
+      name: 'Vue.js',
+      category: 'js',
+    },
+    {
+      name: 'Angular',
+      category: 'js',
+    },
+    {
+      name: 'Redux',
+      category: 'js',
+    },
+    {
+      name: 'Vuex',
+      category: 'js',
+    },
+    {
+      name: 'Svelte',
+      category: 'js',
+    },
+    {
+      name: 'Knockout.js',
+      category: 'js',
+    },
+    {
+      name: 'Backbone.js',
+      category: 'js',
+    },
+    {
+      name: 'TypeScript',
+      category: 'js',
+    },
+    {
+      name: 'D3',
+      category: 'js',
+    },
+    {
+      name: 'Gulp',
+      category: 'tool',
+    },
+    {
+      name: 'Npm',
+      category: 'tool',
+    },
+    {
+      name: 'Webpack',
+      category: 'tool',
+    },
+    {
+      name: 'Mocha',
+      category: 'tool',
+    },
+    {
+      name: 'Jest',
+      category: 'tool',
+    },
+    {
+      name: 'Cypress',
+      category: 'tool',
+    },
+    {
+      name: 'Git',
+      category: 'tool',
+    },
+    {
+      name: 'Unix',
+      category: 'tool',
+    },
+    {
+      name: 'Jekyll',
+      category: 'tool',
+    },
+    {
+      name: 'ReactQuery',
+      category: 'js',
+    },
+    {
+      name: 'PlayWright',
+      category: 'tool',
+    },
+    {
+      name: 'StyledComponents',
+      category: 'js',
+    },
+    {
+      name: 'Zustand',
+      category: 'js',
+    },
+    {
+      name: 'Atomic Design',
+      category: 'js',
+    },
+    {
+      name: 'Figma',
+      category: 'tool',
+    },
+    {
+      name: 'Bulma',
+      category: 'css',
+    },
+    {
+      name: 'Semantic UI',
+      category: 'css',
+    },
+    {
+      name: 'Material UI',
+      category: 'css',
+    },
+    {
+      name: 'Twitter Bootstrap',
+      category: 'css',
+    },
+    {
+      name: 'Responsive',
+      category: 'tech',
+    },
+    {
+      name: 'Mobile Web App',
+      category: 'tech',
+    },
+    {
+      name: 'PWA',
+      category: 'tech',
+    },
+    {
+      name: 'Node.js',
+      category: 'language',
+    },
+    {
+      name: 'Python',
+      category: 'language',
+    },
+    {
+      name: 'Php',
+      category: 'language',
+    },
+    {
+      name: 'Express.js',
+      category: 'backend-framework',
+    },
+    {
+      name: 'Django',
+      category: 'backend-framework',
+    },
+    {
+      name: 'Wordpress',
+      category: 'backend-framework',
+    },
+    {
+      name: 'SQLite',
+      category: 'database',
+    },
+    {
+      name: 'MongoDB',
+      category: 'database',
+    },
+  ],
+  hobbies: ['Reading', 'Coding', 'Badminton', 'Cycling', 'Traveling'],
+};
